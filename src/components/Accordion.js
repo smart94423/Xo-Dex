9aa4f008df57840691d38f0a969d0fcaea2ea98d
smart94423 <smart94423@gmail.com>
2023-05-15 22:45:24 -0400

    complted landing

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -10,8 +10,8 @@ function Accordion(props) {
 
   return (
     <div className='faq_accordion'>
-      <button className={`accordion ${isActive ? 'active' : ''} text-xl font-bold text-gray-600`} onClick={toggleAccordion}  style={{backgroundColor: "rgba(0, 0, 0, 0.5)", color: "white"}}>{props.title}</button>
-      <div className="text-left panel text-sm text-white font-black" style={{ display: isActive ? 'block' : 'none', backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+      <button className={`accordion ${isActive ? 'active' : ''} text-lg font-bold text-gray-600 rounded-xl my-5`} onClick={toggleAccordion}  style={{backgroundColor: "rgba(0, 0, 0, 0.5)", color: "white"}}>{props.title}</button>
+      <div className="text-left panel text-sm text-gray-400 rounded-xl" style={{ display: isActive ? 'block' : 'none', backgroundColor: "rgba(0, 0, 0, 0.25)" }}>
         {props.content}
       </div>
     </div>
